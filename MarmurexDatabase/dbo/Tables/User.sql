1ab38f3949c0133650d00094e8407bc82a8c5cf1
Dariusz Strych <dariusz.strych.lib@gmail.com>
2020-03-06 15:44:36 +0100

    Stored Procedure

--- MarmurexDatabase/dbo/Tables/User.sql
+++ MarmurexDatabase/dbo/Tables/User.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[User]
 ( 
-    [UserId] NVARCHAR(128) NOT NULL PRIMARY KEY, 
-    [FistName] NVARCHAR(50) NOT NULL, 
+    [Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL, 
     [EmailAddress] NVARCHAR(256) NOT NULL, 
     [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
---
+++ MarmurexTCRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace MarmurexTCRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        // GET: User/Details/5
+        public List<UserModel> GetById()
+        {
+            string uderId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData userData = new UserData();
+
+            return userData.GetUserById(uderId);
+        }
+    }
+}
