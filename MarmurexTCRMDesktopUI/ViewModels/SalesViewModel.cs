8338ab209a8fa6464e64df42f8b87871393c095f
Dariusz Strych <dariusz.strych.lib@gmail.com>
2020-03-16 13:26:26 +0100

    Cart ui implementation

--- MarmurexTCRMDesktopUI/ViewModels/SalesViewModel.cs
+++ MarmurexTCRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -10,8 +10,8 @@ using MarmurexTCRMDesktopUI.Library.Models;
 
 namespace MarmurexTCRMDesktopUI.ViewModels
 {
-    public class SalesViewModel : Screen
-    {
+	public class SalesViewModel : Screen
+	{
 		IProductEndpoint _productEndpoint;
 
 		public SalesViewModel(IProductEndpoint productEndpoint)
@@ -24,6 +24,7 @@ namespace MarmurexTCRMDesktopUI.ViewModels
 			var productList = await _productEndpoint.GetAll();
 			Products = new BindingList<ProductModel>(productList);
 		}
+
 		protected override async void OnViewLoaded(object view)
 		{
 			base.OnViewLoaded(view);
@@ -35,34 +36,48 @@ namespace MarmurexTCRMDesktopUI.ViewModels
 		public BindingList<ProductModel> Products
 		{
 			get { return _products; }
-			set 
-			{ 
+			set
+			{
 				_products = value;
 				NotifyOfPropertyChange(() => Products);
 			}
 		}
 
-		private BindingList<ProductModel> _cart;
+		private ProductModel _selectedProduct;
 
-		public BindingList<ProductModel> Cart
+		public ProductModel SelectedProduct
+		{
+			get { return _selectedProduct; }
+			set
+			{
+				_selectedProduct = value;
+				NotifyOfPropertyChange(() => SelectedProduct);
+				NotifyOfPropertyChange(() => CanAddToCart);
+			}
+		}
+
+		private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+		public BindingList<CartItemModel> Cart
 		{
 			get { return _cart; }
-			set 
-			{ 
+			set
+			{
 				_cart = value;
 				NotifyOfPropertyChange(() => Cart);
 			}
 		}
 
-		private int _itemQuantity;
+		private int _itemQuantity = 1;
 
 		public int ItemQuantity
 		{
 			get { return _itemQuantity; }
-			set 
-			{ 
+			set
+			{
 				_itemQuantity = value;
 				NotifyOfPropertyChange(() => ItemQuantity);
+				NotifyOfPropertyChange(() => CanAddToCart);
 			}
 		}
 
@@ -70,7 +85,14 @@ namespace MarmurexTCRMDesktopUI.ViewModels
 		{
 			get
 			{
-				return "$0.00";
+				decimal subTotal = 0;
+
+				foreach (var item in Cart)
+				{
+					subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+				}
+
+				return subTotal.ToString("C");
 			}
 		}
 
@@ -96,13 +118,40 @@ namespace MarmurexTCRMDesktopUI.ViewModels
 			{
 				bool output = false;
 
+				if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+				{
+					output = true;
+				}
+
 				return output;
 			}
 		}
 
 		public void AddToCart()
 		{
-			
+			CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+			if(existingItem != null)
+			{
+				existingItem.QuantityInCart += ItemQuantity;
+
+				Cart.Remove(existingItem);
+				Cart.Add(existingItem);
+			}
+			else
+			{
+				CartItemModel item = new CartItemModel
+				{
+					Product = SelectedProduct,
+					QuantityInCart = ItemQuantity
+				};
+
+				Cart.Add(item);
+			}
+
+			SelectedProduct.QuantityInStock -= ItemQuantity;
+			ItemQuantity = 1;
+			NotifyOfPropertyChange(() => SubTotal);
 		}
 
 		public bool CanRemoveFromCart
